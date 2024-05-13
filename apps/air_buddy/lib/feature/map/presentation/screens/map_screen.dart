af0f110dab4ce2fb4bc277b877880c1046a51fcf
tanawat.k <tanawat.k@orbitdigital.co.th>
2024-05-14 03:20:18 +0700

    feat: create simple map.

---
+++ apps/air_buddy/lib/feature/map/presentation/screens/map_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          FlutterMap(
+            options: MapOptions(
+              center: LatLng(18.793857473522774, 98.97889434756851),
+              zoom: 15,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+
+  }
+}
