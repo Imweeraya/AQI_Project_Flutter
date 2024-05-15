5768f5a2780b57843555d9447793566e86a46563
tle0508 <tlezazazazaza@gmail.com>
2024-05-15 14:55:52 +0700

    add image

--- apps/core/lib/layouts/modal_info.dart
+++ apps/core/lib/layouts/modal_info.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+
 
 class ModalInfo extends StatelessWidget {
   const ModalInfo({super.key});
@@ -14,17 +17,17 @@ class ModalInfo extends StatelessWidget {
         ),
       ),
       content: SizedBox(
-        width: 600, 
-        height: 800, 
+        width: 600,
+        height: 500,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            _buildCustomContainer('Good', '0-50', Color.fromARGB(218, 91, 196, 35)),
-            _buildCustomContainer('Moderate', '51-100', Color.fromARGB(203, 244, 224, 49)),
-            _buildCustomContainer('Unhealthy for Sensitive Groups', '101-150', Color.fromARGB(208, 227, 145, 20)),
-            _buildCustomContainer('Unhealthy', '151-200', const Color.fromARGB(211, 244, 67, 54)),
-            _buildCustomContainer('Very Unhealthy', '201-300', Color.fromARGB(204, 155, 39, 176)),
-            _buildCustomContainer('Hazardous', '301+', Color.fromARGB(214, 121, 85, 72)),
+            _buildCustomContainer('Good', '0-50', '/Users/chitchanok/Documents/GitHub/AQI_Project_Flutter/apps/core/assets/1.PNG',Color.fromARGB(255, 41, 151, 27)),
+            _buildCustomContainer('Moderate', '51-100', '/Users/chitchanok/Documents/GitHub/AQI_Project_Flutter/apps/core/assets/2.PNG',Color.fromARGB(255, 233, 235, 87)),
+            _buildCustomContainer('Unhealthy for Sensitive Groups', '101-150', '/Users/chitchanok/Documents/GitHub/AQI_Project_Flutter/apps/core/assets/3.PNG',Color.fromARGB(255, 245, 118, 34)),
+            _buildCustomContainer('Unhealthy', '151-200', '/Users/chitchanok/Documents/GitHub/AQI_Project_Flutter/apps/core/assets/4.PNG',Color.fromARGB(255, 218, 46, 46)),
+            _buildCustomContainer('Very Unhealthy', '201-300', '/Users/chitchanok/Documents/GitHub/AQI_Project_Flutter/apps/core/assets/5.PNG',Color.fromARGB(255, 96, 7, 125)),
+            _buildCustomContainer('Hazardous', '301+', '/Users/chitchanok/Documents/GitHub/AQI_Project_Flutter/apps/core/assets/6.PNG',Color.fromARGB(255, 101, 12, 12)),
           ],
         ),
       ),
@@ -39,11 +42,11 @@ class ModalInfo extends StatelessWidget {
     );
   }
 
-  Widget _buildCustomContainer(String text1, String text2, Color color) {
+  Widget _buildCustomContainer(String text1, String text2, String imagePath,Color color) {
     return Container(
-      width: 350,  
+      width: 350,
       height: 70,
-      padding: const EdgeInsets.symmetric(horizontal: 5),  
+      padding: const EdgeInsets.symmetric(horizontal: 5),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
         color: Colors.white,
@@ -56,17 +59,13 @@ class ModalInfo extends StatelessWidget {
           ),
         ],
       ),
-      margin: const EdgeInsets.only(bottom: 10.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(
+          Image.asset(
+            imagePath,
             width: 55,
             height: 55,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: color,
-            ),
           ),
           Container(
             width: 230,
