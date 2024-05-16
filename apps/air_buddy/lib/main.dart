d8dbe58be7130f266fd950b13acfd291fe74322e
tanawat.k <tanawat.k@orbitdigital.co.th>
2024-05-16 15:35:22 +0700

    feat: implement merge config.

--- apps/air_buddy/lib/main.dart
+++ apps/air_buddy/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:air_buddy/feature/map/presentation/screens/map_screen.dart';
 import 'package:air_buddy/infrastructure/dependency_injection/inject.dart';
 import 'package:core_libs/dependency_injection/get_it.dart';
-import 'package:air_buddy/infa.dart/injection.dart';
-import 'package:air_buddy/infa.dart/router/router.dart';
+import 'package:air_buddy/infrastructure/router/router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,26 +13,11 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     const Directionality(
-      textDirection: TextDirection.ltr, // Or TextDirection.rtl, depending on your app's language direction
+      textDirection: TextDirection.ltr,
+      // Or TextDirection.rtl, depending on your app's language direction
       child: ProviderScope(
         child: MapScreen(),
       ),
     ),
-  );}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MapScreen(),
-    );
-  }
+  );
 }
