f5f7da89e1d0dc0070dbd6dbd150112eef762a9b
tanawat.k <tanawat.k@orbitdigital.co.th>
2024-05-17 13:58:33 +0700

    test: get weather by city test.

--- apps/air_buddy/test/get_weather_by_city_test.dart
+++ apps/air_buddy/test/get_weather_by_city_test.dart
@@ -480,5 +480,8 @@ void main() {
     final weather = await weatherCityService.getWeatherForecast('Chiang Mai');
 
     expect(weather[0].pollution.aqi, 50);
+    expect(weather[0].pollution.avgUvi, 0);
+    expect(weather[0].weather.maxTemp, 38.0);
+    expect(weather[0].weather.minTemp, 28.0);
   });
 }
