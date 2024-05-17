c3dd2a02ba4a225f48f7295065863327d163f8df
Weeraya Kosandilok <weeraya.kosandilok@clbs.co.th>
2024-05-17 13:56:42 +0200

    fix overflow

--- apps/air_buddy/lib/weather_feature/presentation/widget/list/city_list.dart
+++ apps/air_buddy/lib/weather_feature/presentation/widget/list/city_list.dart
@@ -1,4 +1,3 @@
-
 import 'package:air_buddy/weather_feature/domain/entities/polution_entity.dart';
 import 'package:air_buddy/weather_feature/viewmodel/weather_viewmodel.dart';
 import 'package:core/constants/aqi/aqi_data.dart';
@@ -11,74 +10,75 @@ import 'package:core_ui/widgets/elements/tests/title_text.dart';
 import '../../../domain/entities/weather_city.dart';
 
 class CityList extends ConsumerWidget {
-  const CityList({super.key , required this.city });
+  const CityList({super.key, required this.city});
   final WeatherCity city;
 
   @override
-  Widget build(BuildContext context , WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final weatherVMNotifier = ref.read(weatherViewModelProvider.notifier);
-    final AqiData aqiData = weatherVMNotifier.getAqiData(city.aqi!=null ?int.parse(city.aqi!): 0);
+    final AqiData aqiData = weatherVMNotifier
+        .getAqiData(city.aqi != null ? int.parse(city.aqi!) : 0);
 
     return InkWell(
-      onTap: (){
+      onTap: () {
         weatherVMNotifier.goInfoScreenByCityName(context, city.stationName!);
       },
       child: Container(
-              height: 90,
-              margin: const EdgeInsets.only(top: 20.0, bottom: 20),
-              decoration: BoxDecoration(
-                color: aqiData.backgroundColor,
-                borderRadius: BorderRadius.circular(15.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    spreadRadius: 2,
-                    blurRadius: 4,
-                    offset: const Offset(0, 2),
+        height: 90,
+        margin: const EdgeInsets.only(top: 20.0, bottom: 20),
+        decoration: BoxDecoration(
+          color: aqiData.backgroundColor,
+          borderRadius: BorderRadius.circular(15.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 4,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              NormalText(
+                title: city.stationName ?? "",
+                textSize: TextSize.NORMAL,
+                color: aqiData.textColor,
+                maxLine: 1,
+                overFlow: false,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  NormalText(
+                    title: "AQI",
+                    textSize: TextSize.NORMAL,
+                    color: aqiData.textColor,
+                    textAlign: TextAlign.center,
+                  ),
+                  TitleText(
+                    title: city.aqi.toString(),
+                    textSize: TitleTextSize.REGULAR,
+                    color: aqiData.textColor,
+                    fontWeight: FontWeight.w500,
                   ),
                 ],
               ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text(
-                      city.stationName ?? "",
-                      style: TextStyle(
-                        color: aqiData.textColor,
-                        fontSize: 16,
-                      ),
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                         NormalText(
-                      title: "AQI",
-                      textSize: TextSize.NORMAL,
-                      color:  aqiData.textColor,
-                      textAlign: TextAlign.center,
-                     ),
-                      TitleText(
-                      title: city.aqi.toString(),
-                      textSize: TitleTextSize.REGULAR,
-                      color: aqiData.textColor,
-                      fontWeight: FontWeight.w500,
-                      ),
-                      ],
-                    ),
-                    SvgPicture.asset(
-                      aqiData.pathIcon,
-                      width: 60,
-                      height: 60,
-                      colorFilter: ColorFilter.mode(
-                          aqiData.textColor, BlendMode.srcIn),
-                    ),
-                  ],
-                ),
+              SvgPicture.asset(
+                aqiData.pathIcon,
+                width: 60,
+                height: 60,
+                colorFilter:
+                    ColorFilter.mode(aqiData.textColor, BlendMode.srcIn),
               ),
-            ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
